84646f40966ca358d4c3c30b2ca0ce875b768310
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-10-22 06:12:51 -0500

    added HelloWorld export in lit-app.ts

--- src/lit-app.ts
+++ src/lit-app.ts
@@ -3,6 +3,8 @@ import { customElement, property } from "lit/decorators.js";
 import { provide } from "@lit/context";
 import { MyContext, myContext } from "./my-context.js";
 
+export { HelloWorld } from "./hello-world.js";
+
 @customElement("lit-app")
 export class LitApp extends LitElement {
   @provide({ context: myContext })
