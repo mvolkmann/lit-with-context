371f136dd4928e140e785534946b4d664c7bf82e
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-10-24 12:44:55 -0500

    progress

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,17 +1,15 @@
 import { LitElement, html } from "lit";
-import { customElement, property } from "lit/decorators.js";
+import { customElement, state } from "lit/decorators.js";
 import { consume } from "@lit/context";
 import { MyContext, myContext } from "./my-context.js";
 
 @customElement("hello-world")
 export class HelloWorld extends LitElement {
   @consume({ context: myContext, subscribe: true })
-  @property({ attribute: false })
+  @state()
   private context?: MyContext;
 
   render() {
-    //TODO: Why is this.context undefined here?
-    console.log("hello-world.ts render: this.context =", this.context);
     return html`<p>Hello, ${this.context?.name}!</p>`;
   }
 }
--- src/lit-app.ts
+++ src/lit-app.ts
@@ -3,33 +3,36 @@ import { customElement } from "lit/decorators.js";
 import { provide } from "@lit/context";
 import { MyContext, myContext } from "./my-context.js";
 
-export { HelloWorld } from "./hello-world.js";
-
 @customElement("lit-app")
 export class LitApp extends LitElement {
   @provide({ context: myContext })
-  private context: MyContext = {
-    name: "World",
-    setName(name: string) {
-      this.name = name;
-    },
-  };
+  private context!: MyContext;
 
-  handleClick() {
-    this.context = {
-      name: "World",
-      setName(name: string) {
-        this.name = name;
-      },
-    };
+  constructor() {
+    super();
+    this.setName("World");
   }
 
   render() {
     return html`
       <div>
         <slot></slot>
-        <button @click=${this.handleClick}>Reset</button>
+        <button @click=${this.reset}>Reset</button>
       </div>
     `;
+  }
+
+  reset() {
+    this.setName("World");
+  }
+
+  private setName(name: string) {
+    this.context = {
+      name,
+      //setName: this.setName.bind(this),
+      setName(name: string) {
+        this.name = name;
+      },
+    };
   }
 }
