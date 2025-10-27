1b1d82b473b32042026b51161cada51ada676363
Mark Volkmann <r.mark.volkmann@gmail.com>
2025-10-27 12:19:28 -0500

    progress

--- src/hello-world.ts
+++ src/hello-world.ts
@@ -1,10 +1,18 @@
-import { LitElement, html } from "lit";
+import { css, html, LitElement } from "lit";
 import { customElement, state } from "lit/decorators.js";
 import { consume } from "@lit/context";
 import { MyContext, myContext } from "./my-context.js";
 
 @customElement("hello-world")
 export class HelloWorld extends LitElement {
+  static styles = [
+    css`
+      p {
+        color: purple;
+      }
+    `,
+  ];
+
   @consume({ context: myContext, subscribe: true })
   @state()
   private context?: MyContext;
--- src/labeled-input.ts
+++ src/labeled-input.ts
@@ -1,4 +1,4 @@
-import { LitElement, html, css } from "lit";
+import { css, html, LitElement } from "lit";
 import { customElement, property, state } from "lit/decorators.js";
 import { consume } from "@lit/context";
 import { MyContext, myContext } from "./my-context.js";
